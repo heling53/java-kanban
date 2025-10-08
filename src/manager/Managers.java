0823d902943fd31c9c3d266e519fe54355185350
Happysol <Heling53@yandex.ru>
2025-10-08 18:09:50 +0300

    Ок

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -4,10 +4,11 @@ import manager.impl.InMemoryHistoryManager;
 import manager.impl.InMemoryTaskManager;
 
 public class Managers {
-    public static TaskManager getDefault(){
+    public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
-    public static HistoryManager getDefaultHistory(){
+
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 
