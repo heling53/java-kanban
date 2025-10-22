57a8b96bb6c6c1a2f23903ac89032e8e8e5d0d68
Happysol <Heling53@yandex.ru>
2025-10-22 12:42:45 +0300

    Добавлен taskType

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,9 +1,13 @@
 package tasks;
 
+import tasks.enm.TaskType;
+
 public class Task extends AbstractTask {
 private String title;
     public Task(String name, String description) {
+
         super(name, description);
+        this.taskType = TaskType.TASK;
     }
 
 
