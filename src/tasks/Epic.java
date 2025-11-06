b43a2f7ee6c635c9e2ef86924f5b36ba4454c8fc
Happysol <Heling53@yandex.ru>
2025-11-06 15:18:19 +0300

    Добавлен duration,startTime

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -2,15 +2,19 @@ package tasks;
 
 import tasks.enm.TaskType;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Epic extends AbstractTask {
     private final List<Integer> subTasksId = new ArrayList<>();
+    private LocalDateTime endTime;
 
     public Epic(String name, String description) {
         super(name, description);
         this.taskType = TaskType.EPIC;
+        this.duration = Duration.ZERO;
 
     }
 
@@ -30,6 +34,30 @@ public class Epic extends AbstractTask {
         this.subTasksId.clear();
     }
 
+    public void updateTimeData(List<SubTask> subTasks) {
+        if (subTasks.isEmpty() || subTasks == null) {
+            this.startTime = null;
+            this.endTime = null;
+            this.duration = Duration.ZERO;
+            return;
+        }
+
+        this.startTime = subTasks.stream()
+                .map(SubTask::getStartTime)
+                .filter(t -> t != null)
+                .min(LocalDateTime::compareTo)
+                .orElse(null);
+        this.endTime = subTasks.stream()
+                .map(SubTask::getEndTime)
+                .filter(t -> t != null)
+                .max(LocalDateTime::compareTo)
+                .orElse(null);
+        this.duration = subTasks.stream()
+                .map(SubTask::getDuration)
+                .filter(d -> d != null)
+                .reduce(Duration.ZERO, Duration::plus);
+    }
+
     @Override
     public String toString() {
         return "tasks.Epic{" +
@@ -38,6 +66,9 @@ public class Epic extends AbstractTask {
                 ", description='" + description + '\'' +
                 ", id=" + id +
                 ", type=" + type +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", duration=" + duration +
                 '}';
     }
 }
